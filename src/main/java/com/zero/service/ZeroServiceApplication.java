8b9dd1d086ae29c730039edb0ac3e04d1e654871
naekang <rlawlsgh6306@gmail.com>
2022-07-25 20:02:26 +0900

    feat: Initial Settings
    - 초기 세팅

---
+++ src/main/java/com/zero/service/ZeroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zero.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zero/service/ZeroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
